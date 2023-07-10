76bf82852736975fcc65c0e18d38e34e34ff78d2
Yegorikk <0yegorstep0@gmail.com>
2023-07-10 11:11:44 +0300

    hash

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import { BoardPage } from './pages/BoardPage';
 import { EndingPage } from './pages/EndingPage';
 import { StartingPage } from './pages/StartingPage';
 
 function App() {
-
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<StartingPage />} />
         <Route exact path="/game" element={<BoardPage />} />
         <Route exact path="/end" element={<EndingPage />} />
-        <Route
-          path="*"
-          element={<Navigate to="/tic-tac-react/" replace />}
-        />
+        <Route path="*" element={<Navigate to="/tic-tac-react/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 

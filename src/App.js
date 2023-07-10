43d6803f5ccff890bf59ebbd8ff40c947b0c6d9b
Yegorikk <0yegorstep0@gmail.com>
2023-07-10 10:15:29 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<StartingPage />} />
-        <Route exact path="/game" element={<BoardPage />} />
-        <Route exact path="/end" element={<EndingPage />} />
+        <Route path="tic-tac-react/" element={<StartingPage />} />
+        <Route exact path="tic-tac-react/game" element={<BoardPage />} />
+        <Route exact path="tic-tac-react/end" element={<EndingPage />} />
         <Route
           path="*"
-          element={<Navigate to="/" replace />}
+          element={<Navigate to="tic-tac-react/" replace />}
         />
       </Routes>
     </BrowserRouter>
